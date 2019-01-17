72e217a53418d63ff1a355ba9357ede11be44f4f
tanyagriego <tanya.griego@gmail.com>
2019-01-16 21:41:39 -0800

    more work on get router

--- businesses/router.js
+++ businesses/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const jsonParser = bodyParser.json();
 
 // Post to register a new log
-router.post('/', jsonParser, (req, res) => {
+router.post('', jsonParser, (req, res) => {
   //console.log("this is my test");
   return Business
     .create({
@@ -25,21 +25,22 @@ router.post('/', jsonParser, (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
-  console.log("get request", JSON.stringify(req.params, null, 4));
-   Business
-    .find({business_name: "Groundbreak"})
+router.get('', (req, res) => {
+  console.log("get request", req.query);
+  
+   return Business
+    .find({business_name: req.query.name})
     .then(business_name_result => res.json(business_name_result))
     .catch(err => {
       res.status(500).json({error: err.message});
     })
 });
 
-router.delete('/', (req, res) => {
+router.delete('', (req, res) => {
   return res.json('test string');
 });
 
-router.put('/', (req, res) => {
+router.put('', (req, res) => {
   return res.json('test string');
 });
 
