93debacda20fab928b648e00ad99140ff608fad7
tanyagriego <tanya.griego@gmail.com>
2019-02-05 20:01:32 -0800

    Added CRUD function outlines for front end

--- public/index.js
+++ public/index.js
@@ -1,10 +1,38 @@
-//This function registers a click event and grabs the user's input
+//This function submits a GET request/registers a click event and grabs the user's input
 function submit () {
     $(".submit-button").submit (event => {
         event.preventDefault();
     })
 }
 
+//This function submits a DELETE request for beers
+function deleteBeer() {
+
+}
+//This function submits a DELETE request for businesses
+function deleteBusiness() {
+    
+}
+
+//This function submits a PUT request for beers
+function updateBeer() {
+
+}
+
+//This function submits a PUT request for businesses
+function updateBusiness() {
+
+}
+
+//This function submits a POST request for beers
+function addBeer() {
+
+}
+
+//This function submits a POST request for businesses
+function addBusiness() {
+
+}
 
 //This function creates a container for the business data which is eventually appended to the DOM
 function appendBuinessData (business) {
