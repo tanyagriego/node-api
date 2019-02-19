ba888e414fcdeede3877abc9f237c38cce483e4f
tanyagriego <tanya.griego@gmail.com>
2019-02-18 16:36:48 -0800

    started delete request

--- public/beers.js
+++ public/beers.js
@@ -16,22 +16,39 @@ function renderBeers(beers) {
   beers.forEach(beer => {
     console.log('beer: ', beer);
     $( "#beersList" ).append(
-      `<ul class="beer-list-results">
-        <li _id=${beer._id} class= "beer-list-item">${beer.display_name}</li>
-        <li _id=${beer._id} class= "beer-list-item">${beer.brewer_name}</li>
-        <li _id=${beer._id} class= "beer-list-item">${beer.associated_business}</li>
-        <li _id=${beer._id} class= "beer-list-item">${beer.on_draft}</li></br>
-       </ul>`
+      `<div class="beer-list-results">
+        <ul>
+          <li _id=${beer._id} class= "beer-list-item">${beer.display_name}</li>
+          <li _id=${beer._id} class= "beer-list-item">${beer.brewer_name}</li>
+          <li _id=${beer._id} class= "beer-list-item">${beer.associated_business}</li>
+          <li _id=${beer._id} class= "beer-list-item">${beer.on_draft}</li></br>
+        </ul>
+        <button type="button" class="delete">Delete</button>
+      </div>`
     );   
      // code to create an event listener
   });
 }
 
 // code to delete from database
-function deleteBeer(beer, url) {
-  return fetch(url + '/' + beer, {
+function deleteRequest(beer, url) {
+  let searchTermValue = localStorage.getItem('searchTermVal');
+  searchTermValue = JSON.parse(searchTermValue);
+  return fetch(`http://localhost:3000/api/beers?type=${searchTermValue}`, {
     method: 'delete',
   })
-  .then(response => response.json());
-  // renderBeers();
+  .then(response => response.json())
+  .then (beer => {
+    deleteBeers(beer);
+  });
+}
+
+//This function submits a DELETE request for beers
+function deleteBeer(beer) {
+  //when the delete button is clicked
+ $(".delete").submit (event => {
+    console.log("Delete function fired");
+    event.preventDefault();
+    $('.beer-list-results').remove()
+});
 }
